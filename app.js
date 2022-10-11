3b5e0a8544f969210d954eb561a2837e318aea82
FlashSampson <flashsampson1991@gmail.com>
2022-10-11 11:05:58 +0100

    updated testing to catch err msg for 404 error

--- app.js
+++ app.js
@@ -6,8 +6,20 @@ app.use(express.json());
 
 app.get("/api/categories", getCategories);
 
+app.all("/*", (req, res)=>{
+    res.status(404).send({ msg: "route not found" });
+})
+
 app.use((err, req, res, next) => {
-    res.status(500).send({ msg: "Oops server broke :(" });
-  });
+  if (err.status) {
+    res.status(err.status).send({msg: err.msg});
+  } else {
+    next(err);
+  }
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Oops server broke :(" });
+});
 
 module.exports = app;
